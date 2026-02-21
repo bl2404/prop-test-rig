4a1a1508b37f731b7d0008f2f8b91d7f5b811f99
bl2404 <bruleb24@gmail.com>
2026-02-21 11:15:00 +0000

    adc for current sensor

--- main/main.c
+++ main/main.c
@@ -6,16 +6,22 @@
 #include <driver/gpio.h>
 #include <sdkconfig.h>
 #include <mpu6050.h>
+#include <esp_adc/adc_oneshot.h>
 
-#define LED_PIN 10
-#define TSCK3 2
-#define TDA3 3
+#define LED_PIN 8
+#define TSCK3 10
+#define TDA3 9
 #define TSCK2 4
 #define TDA2 5
 
 #define ADDR MPU6050_I2C_ADDRESS_LOW
 
-static const char *TAG = "hx711-example";
+#define ADC_PIN ADC_CHANNEL_2
+#define ADC_UNIT ADC_UNIT_1
+#define ADC_BITWIDTH ADC_BITWIDTH_DEFAULT // 12-bit resolution (0-4095)
+#define ADC_ATTEN ADC_ATTEN_DB_12         // ~3.3V full-scale voltage
+
+static const char *TAG = "prop-test-rig";
 
 int32_t get_tenso_data(hx711_t *tenso)
 {
@@ -43,6 +49,7 @@ void test(void *pvParameters)
             .pd_sck = TSCK3,
             .gain = HX711_GAIN_A_64};
     ESP_ERROR_CHECK(hx711_init(&tenso3));
+
     hx711_t tenso2 =
         {
             .dout = TDA2,
@@ -118,9 +125,41 @@ void mpu6050_test(void *pvParameters)
     }
 }
 
+void adc_read(void *pvParameters)
+{
+    int adc_value;
+    adc_oneshot_unit_handle_t adc_handle;
+
+    // Initialize ADC Oneshot Mode Driver on the ADC Unit
+    adc_oneshot_unit_init_cfg_t init_config = {
+        .unit_id = ADC_UNIT,
+        .clk_src = ADC_DIGI_CLK_SRC_DEFAULT};
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config, &adc_handle));
+
+    // Configure ADC channel
+    adc_oneshot_chan_cfg_t config = {
+        .bitwidth = ADC_BITWIDTH,
+        .atten = ADC_ATTEN,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc_handle, ADC_PIN, &config));
+
+    // ADC Oneshot Analog Read loop
+    while (1)
+    {
+        // Read ADC value with Oneshot
+        ESP_ERROR_CHECK(adc_oneshot_read(adc_handle, ADC_PIN, &adc_value));
+        // Print ADC value
+        float voltage = (float)adc_value / 4096.0 * 3.3;
+        ESP_LOGI("Voltage", "%f", voltage);
+        // Delay 1 second
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
 void app_main()
 {
     xTaskCreate(test, "test", configMINIMAL_STACK_SIZE * 5, NULL, 5, NULL);
-    // xTaskCreate(blink_led, "blink_led", configMINIMAL_STACK_SIZE * 2, NULL, 4, NULL);
-    // xTaskCreate(mpu6050_test, "mpu6050_test", configMINIMAL_STACK_SIZE * 6, NULL, 5, NULL);
+    // xTaskCreate(adc_read, "adc_read", configMINIMAL_STACK_SIZE * 5, NULL, 5, NULL);
+    //  xTaskCreate(blink_led, "blink_led", configMINIMAL_STACK_SIZE * 2, NULL, 4, NULL);
+    //  xTaskCreate(mpu6050_test, "mpu6050_test", configMINIMAL_STACK_SIZE * 6, NULL, 5, NULL);
 }
