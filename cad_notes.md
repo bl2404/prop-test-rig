167d64bd08bb2e32bd5af86603a6ab228b26738e
bl2404 <bruleb24@gmail.com>
2026-02-22 16:10:34 +0000

    cad files + basic throttle

---
+++ cad_notes.md
@@ -0,0 +1,11 @@
+- mpu6050 dimensions:
+    - inter-hole = 15.56
+    - hole-diam = 3.12
+    - length = 21.11
+    - width = 15.75
+    - right is bolt if pins are towards me
+
+
+- Blue threadlocker - can be removed, red is permament
+- samokontrujace - 2 thread above the nut
+
--- main/main.c
+++ main/main.c
@@ -7,8 +7,10 @@
 #include <sdkconfig.h>
 #include <mpu6050.h>
 #include <esp_adc/adc_oneshot.h>
+#include "driver/rmt_tx.h"
+#include <dshot_esc_encoder.h>
 
-#define LED_PIN 8
+#define LED_PIN 0 // no such a pin!
 #define TSCK3 10
 #define TDA3 9
 #define TSCK2 4
@@ -20,6 +22,9 @@
 #define ADC_UNIT ADC_UNIT_1
 #define ADC_BITWIDTH ADC_BITWIDTH_DEFAULT // 12-bit resolution (0-4095)
 #define ADC_ATTEN ADC_ATTEN_DB_12         // ~3.3V full-scale voltage
+
+#define DSHOT_ESC_RESOLUTION_HZ 40000000 // 40MHz resolution, DSHot protocol needs a relative high resolution
+#define DSHOT_ESC_GPIO_NUM 8
 
 static const char *TAG = "prop-test-rig";
 
@@ -132,7 +137,7 @@ void adc_read(void *pvParameters)
 
     // Initialize ADC Oneshot Mode Driver on the ADC Unit
     adc_oneshot_unit_init_cfg_t init_config = {
-        .unit_id = ADC_UNIT,
+        .unit_id = ADC_UNIT_1,
         .clk_src = ADC_DIGI_CLK_SRC_DEFAULT};
     ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config, &adc_handle));
 
@@ -156,10 +161,89 @@ void adc_read(void *pvParameters)
     }
 }
 
+void throttle(void *pvParameters)
+{
+    int adc_value;
+    adc_oneshot_unit_handle_t adc_handle;
+
+    // Initialize ADC Oneshot Mode Driver on the ADC Unit
+    adc_oneshot_unit_init_cfg_t init_config = {
+        .unit_id = ADC_UNIT_1,
+        .clk_src = ADC_DIGI_CLK_SRC_DEFAULT};
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config, &adc_handle));
+
+    // Configure ADC channel
+    adc_oneshot_chan_cfg_t config = {
+        .bitwidth = ADC_BITWIDTH,
+        .atten = ADC_ATTEN,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc_handle, ADC_CHANNEL_3, &config));
+
+    // ADC Oneshot Analog Read loop
+    // while (1)
+    // {
+    //     // Read ADC value with Oneshot
+    //     ESP_ERROR_CHECK(adc_oneshot_read(adc_handle, ADC_CHANNEL_3, &adc_value));
+    //     // Print ADC value
+    //     float voltage = (float)adc_value / 4096.0 * 100;
+    //     ESP_LOGI("Throttle: ", "%f", voltage);
+    //     // Delay 1 second
+    //     vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // }
+
+    ESP_LOGI(TAG, "Create RMT TX channel");
+    rmt_channel_handle_t esc_chan = NULL;
+    rmt_tx_channel_config_t tx_chan_config = {
+        .clk_src = RMT_CLK_SRC_DEFAULT, // select a clock that can provide needed resolution
+        .gpio_num = DSHOT_ESC_GPIO_NUM,
+        .mem_block_symbols = 64,
+        .resolution_hz = DSHOT_ESC_RESOLUTION_HZ,
+        .trans_queue_depth = 10, // set the number of transactions that can be pending in the background
+    };
+    ESP_ERROR_CHECK(rmt_new_tx_channel(&tx_chan_config, &esc_chan));
+
+    ESP_LOGI(TAG, "Install Dshot ESC encoder");
+    rmt_encoder_handle_t dshot_encoder = NULL;
+    dshot_esc_encoder_config_t encoder_config = {
+        .resolution = DSHOT_ESC_RESOLUTION_HZ,
+        .baud_rate = 300000, // DSHOT300 protocol
+        .post_delay_us = 50, // extra delay between each frame
+    };
+    ESP_ERROR_CHECK(rmt_new_dshot_esc_encoder(&encoder_config, &dshot_encoder));
+
+    ESP_LOGI(TAG, "Enable RMT TX channel");
+    ESP_ERROR_CHECK(rmt_enable(esc_chan));
+
+    rmt_transmit_config_t tx_config = {
+        .loop_count = -1, // infinite loop
+    };
+    dshot_esc_throttle_t throttle = {
+        .throttle = 0,
+        .telemetry_req = false, // telemetry is not supported in this example
+    };
+
+    ESP_LOGI(TAG, "Start ESC by sending zero throttle for a while...");
+    ESP_ERROR_CHECK(rmt_transmit(esc_chan, dshot_encoder, &throttle, sizeof(throttle), &tx_config));
+    vTaskDelay(pdMS_TO_TICKS(5000));
+
+    ESP_LOGI(TAG, "Increase throttle, no telemetry");
+    for (uint16_t thro = 100; thro < 1000; thro += 10)
+    {
+        throttle.throttle = thro;
+        ESP_ERROR_CHECK(rmt_transmit(esc_chan, dshot_encoder, &throttle, sizeof(throttle), &tx_config));
+        // the previous loop transfer is till undergoing, we need to stop it and restart,
+        // so that the new throttle can be updated on the output
+        ESP_ERROR_CHECK(rmt_disable(esc_chan));
+        ESP_ERROR_CHECK(rmt_enable(esc_chan));
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
 void app_main()
 {
-    xTaskCreate(test, "test", configMINIMAL_STACK_SIZE * 5, NULL, 5, NULL);
+    // xTaskCreate(test, "test", configMINIMAL_STACK_SIZE * 5, NULL, 5, NULL);
     // xTaskCreate(adc_read, "adc_read", configMINIMAL_STACK_SIZE * 5, NULL, 5, NULL);
     //  xTaskCreate(blink_led, "blink_led", configMINIMAL_STACK_SIZE * 2, NULL, 4, NULL);
-    //  xTaskCreate(mpu6050_test, "mpu6050_test", configMINIMAL_STACK_SIZE * 6, NULL, 5, NULL);
+    // xTaskCreate(mpu6050_test, "mpu6050_test", configMINIMAL_STACK_SIZE * 6, NULL, 5, NULL);
+    xTaskCreate(throttle, "throttle", configMINIMAL_STACK_SIZE * 5, NULL, 5, NULL);
 }
